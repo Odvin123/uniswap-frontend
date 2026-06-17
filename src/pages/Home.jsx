a42e771d8083143cdaedcf5cbf09a2676581666f
Odvin <odvinenriquerivera12345@gmail.com>
2026-06-17 00:59:41 -0600

    Agregar árbol 3D en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -69,8 +69,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      
-      <Arbol3D />
+    
       
       {/* Stats Section */}
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mb-20">
